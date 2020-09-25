f5b6c70361566f845c4c5b7ef452ff7d6a347a31
litommy03 <lishipei333@163.com>
2020-09-25 18:35:53 +0800

    新增获取位置代码
    addtodo新增获取图片和位置代码

--- miniprogram/pages/addTodo/addTodo.js
+++ miniprogram/pages/addTodo/addTodo.js
@@ -3,32 +3,63 @@ const db = wx.cloud.database();
 const todos = db.collection('todos')
 
 Page({
+  data:{
+    image:null
+  },
+  pageData:{
+    locationObj:{}
+  },
   onSubmit:function(event){
     //console.log(event.detail.value.title)
     todos.add({
       data:{
-        title:event.detail.value.title
+        title:event.detail.value.title,
+        image:this.data.image,
+        location:this.pageData.locationObj
       }
     }).then(res=>{
       wx.showToast({
-        title: 'Success',
-        icon:'success'
+        title: '添加成功',
+        icon:'success',
+        success:res2=>{
+          wx.redirectTo({
+            url: `../todoInfo/todoInfo?id=${res._id}`,
+          })
+        }
       })
     })
   },
   selectImage:function(e){
     wx.chooseImage({
-      success: function(res){
+      //success: function(res){
+        success: res => {
         console.log(res.tempFilePaths[0]);
         wx.cloud.uploadFile({
           cloudPath : `${Math.floor(Math.random()*10000000)}.png`,
           filePath: res.tempFilePaths[0],
         }).then(res=>{
           console.log(res.fileID);
+          this.setData({
+            image: res.fileID
+          })
         }).catch(err=>{
           console.error(err);
         })
       },
+    })
+  },
+  chooseLocation:function(e){
+    wx.chooseLocation({
+      //latitude: 0,
+      success: res=> {
+        let locationObj = {
+          latitude: res.latitude,
+          longitude: res.longitude,
+          name: res.name,
+          address: res.address
+        }
+        this.pageData.locationObj = locationObj
+      }
     })
   }
 })
